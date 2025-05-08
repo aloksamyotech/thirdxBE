6ac023f0298b5e3d924c6df7dd7b34461ea5476f
techteam-01 <techteam@samyotech.com>
2025-05-08 11:00:40 +0530

    all code

---
+++ src/models/mail.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose'
+const MailingListSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    tags: [String],
+    channelSettings: [String],
+    purposeSettings: [String],
+    includeArchived: { type: Boolean, default: false },
+    filters: [
+        {
+            logic: {
+                type: String,
+                enum: ['AND', 'OR'],
+                required: true
+            },
+            field: {
+                type: String,
+                required: true
+            },
+            comparison: {
+                type: String,
+                enum: ['equals', 'not_equals', 'contains', 'not_contains', 'greater_than', 'less_than'],
+                required: true
+            },
+            value: {
+                type: mongoose.Schema.Types.Mixed,
+                required: true
+            }
+        }
+    ],
+},
+    { timestamps: true }
+);
+
+const mail = mongoose.model("mail", MailingListSchema);
+export default mail;
