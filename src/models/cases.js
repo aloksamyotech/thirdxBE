22acdde34bf888e02766c6bbf46bff7d23a3537f
Alok Chaturvedi <alokchaturvedi0145@gmail.com>
2025-05-12 12:45:27 +0530

    done changes in getAllCases api

--- src/models/cases.js
+++ src/models/cases.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const caseSchema = new mongoose.Schema({
   serviceName: { type: String, required: true },
-  serviceCode: { type: String, required: true, unique: true },
+  service: { type: String, required: true },
   serviceType: { type: String, required: true },
   serviceStatus: { type: String, required: true },
   caseOpened: { type: Date },
