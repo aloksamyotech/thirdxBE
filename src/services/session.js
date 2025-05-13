86ea9a65d0aac6731bda6d250705538de4e8e0c8
Jairaj Lakher <jairaj.lakher@samyotech.in>
2025-05-13 11:46:05 +0530

    made session api 5

---
+++ src/services/session.js
@@ -0,0 +1,148 @@
+/* eslint-disable no-unsafe-optional-chaining */
+/* eslint-disable no-undef */
+import { errorCodes, Message, statusCodes } from '../core/common/constant.js'
+import CustomError from '../utils/exception.js'
+import Session from '../models/session.js'
+
+export const addSession = async (req) => {
+  const {
+    name,
+    code,
+    isActive,
+    type,
+    description,
+    benificiary,
+    campaigns,
+    engagement,
+    eventAttanded,
+    fundingInterest,
+    time,
+    date,
+    fundraisingActivities,
+  } = req.body
+
+  const serviceData = {
+    name,
+    code,
+    isActive,
+    type,
+    description,
+    benificiary,
+    campaigns,
+    engagement,
+    time,
+    date,
+    eventAttanded,
+    fundingInterest,
+    fundraisingActivities,
+  }
+
+  if (req.file && req.file.filename) {
+    serviceData.file = `uploads/${req.file.filename}`
+  }
+
+  const newSession = await Session.create(serviceData)
+
+  if (!newSession) {
+    throw new CustomError(
+      statusCodes.badRequest,
+      Message.notCreated,
+      errorCodes.bad_request
+    )
+  }
+
+  return { newSession }
+}
+export const deleteSession = async (req) => {
+  // const { id } = req?.params;
+  const sessionId = req.params.id
+  const serssionData = await Session.findById(serviceId)
+
+  if (!serssionData) {
+    throw new CustomError(
+      statusCodes?.notFound,
+      Message?.notFound,
+      errorCodes?.not_found
+    )
+  }
+  const statusUpdate = await Session.findByIdAndUpdate(
+    sessionId,
+    { isDeleted: true },
+    { new: true }
+  )
+
+  if (!statusUpdate) {
+    throw new CustomError(
+      statusCodes?.notFound,
+      Message?.notUpdate,
+      errorCodes?.not_found
+    )
+  }
+  return { statusUpdate }
+}
+
+export const searchSession = async (req, res) => {
+  const { name, isActive, type } = req.query
+
+  const searchQuery = {}
+  if (name) {
+    searchQuery.name = { $regex: name, $options: 'i' }
+  }
+
+  if (isActive !== undefined) {
+    searchQuery.isActive = isActive === 'true'
+  }
+
+  if (type) {
+    searchQuery.type = { $regex: type, $options: 'i' }
+  }
+
+  const session = await Session.find(searchQuery)
+
+  if (session.length === 0) {
+    return res.status(404).json({
+      success: false,
+      message: 'No session found matching your criteria.',
+    })
+  }
+
+  return res.status(200).json({
+    session,
+  })
+}
+
+export const getSessionById = async (req) => {
+  const serviceId = req?.params.id
+
+  if (!serviceId) {
+    throw new CustomError(
+      statusCodes?.notFound,
+      Message?.notFound,
+      errorCodes?.not_found
+    )
+  }
+
+  const userData = await Session.findOne({ _id: serviceId, isDeleted: false })
+  if (!userData) {
+    throw new CustomError(
+      statusCodes?.notFound,
+      Message?.userNotGet,
+      errorCodes?.user_not_found
+    )
+  }
+  return { userData }
+}
+
+export const getAllSession = async () => {
+  const allSession = await Session.find({ isDeleted: false }).sort({
+    createdAt: -1,
+  })
+  if (!allSession) {
+    throw new CustomError(
+      statusCodes?.notFound,
+      Message?.notFound,
+      errorCodes?.not_found
+    )
+  }
+  return { allSession }
+}
