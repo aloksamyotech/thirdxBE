c6113fdc5f329f92f31ba84533f8d265e338450a
Jairaj Lakher <jairaj.lakher@samyotech.in>
2025-05-15 16:27:58 +0530

    added asynchandler in required places

--- src/routes/case.js
+++ src/routes/case.js
@@ -14,12 +14,9 @@ import { upload } from '../core/helpers/multer.js'
 const router = Router()
 
 router.post('/addCase', upload.single('file'), asyncHandler(addCase));
-
 router.patch('/deleteCase/:id', asyncHandler(deleteCase))
-
-router.get('/search', searchCase)
+router.get('/search', asyncHandler(searchCase))
 router.get('/getCaseById/:id', asyncHandler(getCaseById))
-
 router.get('/getAllCases', asyncHandler(getAllCases))
 
 export default router
