a5ebb36828d31187c58d2d205c5e01f77b41dcef
Diksha <diksha.kushwah@samyotech.in>
2025-05-22 16:23:24 +0530

    resolve

--- src/services/configuration.js
+++ src/services/configuration.js
@@ -169,7 +169,7 @@ export const getConfigurationWithPagination = async (query) => {
     .skip(skip)
     .limit(limitNumber)
     .sort({ createdAt: -1 })
-    .nonDeleted()
+    .notDeleted()
 
   const total = await configuration.countDocuments(filter)
   return {
