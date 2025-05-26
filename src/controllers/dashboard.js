7542c971d099a6e605309fbb18a9380938fa4f5a
Jairaj Lakher <jairaj.lakher@samyotech.in>
2025-05-26 17:50:03 +0530

    dashboard apis

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -5,3 +5,20 @@ export const getAllDonationTotal = async (req, res) => {
   const allMail = await dashboardService.getAllDonationTotal()
   res.status(statusCodes?.ok).send(allMail)
 }
+
+export const getAllSessionDelivered = async (req, res) => {
+  const allMail = await dashboardService.getAllSessionDelivered()
+  res.status(statusCodes?.ok).send(allMail)
+}
+
+
+export const getAllActiveServiceUser = async (req, res) => {
+  const allMail = await dashboardService.getAllActiveServiceUser()
+  res.status(statusCodes?.ok).send(allMail)
+}
+
+export const getAllOpenCased = async (req, res) => {
+  const allMail = await dashboardService.getAllOpenCased()
+  res.status(statusCodes?.ok).send(allMail)
+}
+
