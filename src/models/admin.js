b3becd808e72a75623a7e7d6474867f5b7d937d7
amansamyotech <aman.asati@samyotech,com>
2025-05-28 14:52:12 +0530

    create updated admin api

--- src/models/admin.js
+++ src/models/admin.js
@@ -2,9 +2,42 @@ import mongoose from 'mongoose'
 import { commonFieldsPlugin } from './plugin/commonFields.plugin.js'
 
 const adminSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
   email: {
     type: String,
     required: true,
+  },
+  organization: {
+    type: String,
+  },
+  phoneNumber: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  state: {
+    type: String,
+  },
+  zipCode: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+  language: {
+    type: String,
+  },
+  status: {
+    type: String,
+  },
+  currency: {
+    type: String,
   },
   userName: {
     type: String,
