cbbbb81b82078f6002491de26cefea5ff43f4d1c
Diksha <diksha.kushwah@samyotech.in>
2025-07-18 18:12:20 +0530

    api of find duplicate

---
+++ src/services/duplicate.js
@@ -0,0 +1,40 @@
+import User from '../models/user.js';
+
+export const getDuplicate = async () => {
+  const duplicates = await User.aggregate([
+    {
+      $match: {
+        isDelete: false
+      }
+    },
+    {
+      $group: {
+        _id: {
+          email: "$contactInfo.email",
+          firstName: { $trim: { input: "$personalInfo.firstName" } },
+          lastName: { $trim: { input: "$personalInfo.lastName" } },
+          dob: "$personalInfo.dateOfBirth"
+        },
+        users: { $push: "$$ROOT" },
+        count: { $sum: 1 }
+      }
+    },
+    {
+      $match: {
+        count: { $gt: 1 }
+      }
+    },
+    {
+      $project: {
+        _id: 0,
+        email: "$_id.email",
+        firstName: "$_id.firstName",
+        lastName: "$_id.lastName",
+        dob: "$_id.dob",
+        users: 1
+      }
+    }
+  ]);
+
+  return duplicates;
+};
