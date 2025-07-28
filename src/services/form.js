3192dd978cf092e2103f788bd22e3d11a2a27e97
itzneeraj06 <neerajchouhan0604@gmail.com>
2025-07-28 18:11:19 +0530

    form filter update

--- src/services/form.js
+++ src/services/form.js
@@ -50,19 +50,30 @@ export const getFormById = async (formId) => {
 
 export const getAllForms = async (query) => {
 
-    const { page = 1, limit = 10, search } = query || {}
+    const { page = 1, limit = 10, search, type, title, createdAt } = query || {}
     const skip = (page - 1) * limit;
-    const searchQuery = search ? {
-        title: { $regex: search, $options: 'i' }
-    } : {};
-
+    const filters = {
+        ...(search && { title: { $regex: search, $options: 'i' } }),
+        ...(title && { title: { $regex: title, $options: 'i' } }),
+        ...(type && { type: { $regex: type, $options: 'i' } })
+    };
+    if (createdAt) {
+        const parsedDate = new Date(createdAt)
+        if (!isNaN(parsedDate)) {
+            const start = new Date(parsedDate)
+            start.setHours(0, 0, 0, 0)
+            const end = new Date(parsedDate)
+            end.setHours(23, 59, 59, 999)
+            filters.createdAt = { $gte: start, $lte: end }
+        }
+    }
     const form = await Form
-        .find(searchQuery)
+        .find(filters)
         .skip(skip)
         .limit(limit)
         .sort({ createdAt: -1 });
 
-    const total = await Form.countDocuments(searchQuery);
+    const total = await Form.countDocuments(filters);
     return {
         data: form,
         meta: {
